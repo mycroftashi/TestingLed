e17ab84b56d556bc650711770e31ba745e53ab7e
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-01 20:43:20 -0500

    Update audiototext.py

--- audiototext.py
+++ audiototext.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr # create the recognizer
 r = sr.Recognizer() # define the microphone
-mic = sr.Microphone(device_index=0) # record your speech
+mic = sr.Microphone() # record your speech
 with mic as source:
    audio = r.listen(source) # speech recognition
 result = r.recognize_google(audio)# export the result
